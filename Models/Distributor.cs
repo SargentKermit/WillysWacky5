eefdd1bf377dca63b1062df71f62d0769666c068
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-02 09:10:45 -0600

    distributorTableCreated

---
+++ Models/Distributor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WillysWacky5.Models
+{
+    public class Distributor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DistributorLogoURL { get; set; }
+        public string DistributorName { get; set; }
+        public string DistributorAddress { get; set; }
+    }
+}
