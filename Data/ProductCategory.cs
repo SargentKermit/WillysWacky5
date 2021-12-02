7f3a4355b00f788ae18837668442ef93518915a7
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-02 09:22:51 -0600

    productsMVCCreated

---
+++ Data/ProductCategory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WillysWacky5.Data
+{
+    public enum ProductCategory
+    {
+        Animal = 1,
+        Art,
+        Baby,
+        Cleaning,
+        Clothing,
+        Electronics,
+        Food,
+        Footwear,
+        Furniture,
+        Garden,
+        Health,
+        Home,
+        Jewelry,
+        Media,
+        Office,
+        Sports,
+        Tools,
+        Toys,
+        Vehicle
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WillysWacky5.Data;
+
+namespace WillysWacky5.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductImageURL { get; set; }
+
+        public ProductCategory ProductCategory { get; set; }
+
+    }
+}
