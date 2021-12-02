8ebdd4c6a988544d6f573bacb795459bdd2807f0
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-02 09:14:21 -0600

    shipMVCAdded

---
+++ Models/Ship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WillysWacky5.Models
+{
+    public class Ship
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ShipMapLocationURL { get; set; }
+        public string ShipState { get; set; }
+        public string ShipAddress { get; set; }
+    }
+}
