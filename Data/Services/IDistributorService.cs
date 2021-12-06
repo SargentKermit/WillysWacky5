8d3b6e48cdc7d09b58fd501276f0751cf51d65b6
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-05 21:12:47 -0600

    as interface

---
+++ Data/Services/IDistributorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WillysWacky5.Models;
+
+namespace WillysWacky5.Data.Services
+{
+    public interface IDistributorService
+    {
+        IEnumerable<Distributor> GetAll();
+        Distributor GetById(int id);
+        void Add(Distributor distributor);
+        Distributor Update(int id, Distributor newDistributor);
+        void Delete(int id);
+    }
+}
