613eedd7b7f3a775969208da442734e6c8f7a4d8
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-07 21:10:42 -0600

    update/deletebaserepo

--- Data/Base/EntityBaseRepository.cs
+++ Data/Base/EntityBaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,11 @@ namespace WillysWacky5.Data.Base
             _context = context;
         }
         public  async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
         }
 
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
@@ -24,9 +27,10 @@ namespace WillysWacky5.Data.Base
        
 
 
-        public Task<T> UpdateAsync(int id, T entity)
+        public async Task UpdateAsync(int id, T entity)
         {
-            throw new NotImplementedException();
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
         }
     }
 }
