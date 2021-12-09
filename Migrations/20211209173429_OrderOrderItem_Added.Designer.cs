f55993a4fa373440a7788c811e9e53f0e65da4f5
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-09 11:40:03 -0600

    62completedmigration

--- Migrations/20211209173429_OrderOrderItem_Added.Designer.cs
+++ Migrations/20211209173429_OrderOrderItem_Added.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WillysWacky5.Data;
 
 namespace WillysWacky5.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211209173429_OrderOrderItem_Added")]
+    partial class OrderOrderItem_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
