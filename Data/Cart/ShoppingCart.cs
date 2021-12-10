9fff2266c4a78be17fecfaefab82ff213b5b3720
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-09 21:17:41 -0600

    65additemmethod

--- Data/Cart/ShoppingCart.cs
+++ Data/Cart/ShoppingCart.cs
@@ -18,6 +18,26 @@ namespace WillysWacky5.Data.Cart
         {
             _context = context;
         }
+        public void AddItemToCart(Product product)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Product.Id == product.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Product = product,
+                    Amount = 1
+                };
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
 
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
