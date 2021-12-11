d63b2486bc0ca199db66bcf0e3d71cfc964d32b7
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-11 00:14:21 -0600

    89signupnewuser

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WillysWacky5.Data;
+using WillysWacky5.Data.Static;
 using WillysWacky5.Data.ViewModels;
 using WillysWacky5.Models;
 
@@ -51,6 +52,29 @@ namespace WillysWacky5.Controllers
         }
         public IActionResult Register() => View(new RegisterVM());
 
-        
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
     }
 }
