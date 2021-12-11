c22b395f453b88f3bb699bee40529d08590716f5
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-10 23:07:54 -0600

    84startupocnfigid

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +15,7 @@ using System.Threading.Tasks;
 using WillysWacky5.Data;
 using WillysWacky5.Data.Cart;
 using WillysWacky5.Data.Services;
+using WillysWacky5.Models;
 
 namespace WillysWacky5
 {
@@ -38,7 +41,14 @@ namespace WillysWacky5
             services.AddScoped<IOrdersService, OrdersService>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+            //Authentication and authorization
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+            services.AddMemoryCache();
             services.AddSession();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
             services.AddControllersWithViews();
         }
 
@@ -60,6 +70,10 @@ namespace WillysWacky5
 
             app.UseRouting();
             app.UseSession();
+
+            //Authentication & Authorization
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseAuthorization();
 
