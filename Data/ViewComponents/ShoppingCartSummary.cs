a27732f99f0d575cc58c20873e1aae8aedf8b911
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-10 19:11:23 -0600

    72carticonthing

---
+++ Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WillysWacky5.Data.Cart;
+
+namespace WillysWacky5.Data.ViewComponents
+{
+
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
