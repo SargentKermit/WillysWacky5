38f38bbf805723f99eb077a0e62865cc65b222f4
[Collin Reinemann] <[reinemann05884@gotoltc.edu]>
2021-12-11 01:20:38 -0600

    94rolebaseddddorderveios

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using WillysWacky5.Data.Cart;
 using WillysWacky5.Data.Services;
@@ -22,9 +23,10 @@ namespace WillysWacky5.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            string userId = "";
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userRole = User.FindFirstValue(ClaimTypes.Role);
 
-            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            var orders = await _ordersService.GetOrdersByUserIdAndRoleAsync(userId, userRole);
             return View(orders);
         }
         public IActionResult ShoppingCart()
@@ -63,8 +65,8 @@ namespace WillysWacky5.Controllers
         public async Task<IActionResult> CompleteOrder()
         {
             var items = _shoppingCart.GetShoppingCartItems();
-            string userId = "";
-            string userEmailAddress = "";
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userEmailAddress = User.FindFirstValue(ClaimTypes.Email);
 
             await _ordersService.StoreOrderAsync(items, userId, userEmailAddress);
             await _shoppingCart.ClearShoppingCartAsync();
